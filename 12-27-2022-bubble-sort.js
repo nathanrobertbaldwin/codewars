336cc568494e9bbcc80cec57c343e9b12975ff06
Nathan Baldwin <nathanrobertbaldwin@gmail.com>
2022-12-31 14:19:20 -0600

    Added 12-31-2022-insertion-sort-algorithm.

--- 12-27-2022-bubble-sort.js
+++ 12-27-2022-bubble-sort.js
@@ -1,20 +1,22 @@
+// What's happening here? Array destructuring?
+
 function swapData(array, index1, index2) {
-    let first = array[index1];
-    let next = array[index2];
-    array[index1] = next;
-    array[index2] = first;
-    return array;
+    [array[index1], array[index2]] = [array[index2], array[index1]]
 }
 
+
 function bubbleSort(array) {
+    let noSwaps = true;
     for (let j = array.length - 1; j >= 0; j--) {
         for (let i = 0; i < array.length - 1; i++) {
             let first = array[i];
             let next = array[i + 1];
             if (first > next) {
+                noSwaps = false; // Use the noSwaps variable as a switch. If there weren't any swaps on the last round, break;
                 swapData(array, i, i + 1)
             }
         }
+        if (noSwaps) break;
     }
     return array;
 }
